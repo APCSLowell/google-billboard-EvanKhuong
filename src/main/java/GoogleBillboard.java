ccb1f090ea3c5e6d39d9161e8e1fc3788d573b1f
EvanKhuong <119890669+EvanKhuong@users.noreply.github.com>
2024-01-22 12:35:18 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -17,7 +17,7 @@ public class GoogleBillboard {
 	
 
 	//Finish this function
-	public static boolean isPrime(double dNum){
+	public boolean isPrime(double dNum){
 	if (dNum<10){
     	return false;
   	}
